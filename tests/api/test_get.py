573e9669174b3e582fd2e2317856dd29a765b5b6
Ege Güneş <egegunes@gmail.com>
2018-10-20 00:36:49 +0300

    Fix lint errors

--- tests/api/test_get.py
+++ tests/api/test_get.py
@@ -1,8 +1,7 @@
 import os
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 from redmine.redmine import Redmine
-
 
 redmine = Redmine("http://example.com",
                   "API_KEY",
@@ -25,11 +24,10 @@ def test_get_projects_calls_set_cache_when_no_cache():
     resource = "projects"
     data = {"projects": [{"id": 1, "name": "Project"}]}
 
-    with patch.object(Redmine, "fetch", return_value=data) as mock_fetch:
+    with patch.object(Redmine, "fetch", return_value=data):
         with patch.object(Redmine, "set_cache") as mock_cache:
             os.path.exists = MagicMock(return_value=False)
             redmine.get(resource)
-
 
     cache_file = os.path.join(redmine.cache_dir, "{}.json".format(resource))
 
@@ -51,11 +49,10 @@ def test_get_trackers_calls_set_cache_when_no_cache():
     resource = "trackers"
     data = {"trackers": [{"id": 1, "name": "Tracker"}]}
 
-    with patch.object(Redmine, "fetch", return_value=data) as mock_fetch:
+    with patch.object(Redmine, "fetch", return_value=data):
         with patch.object(Redmine, "set_cache") as mock_cache:
             os.path.exists = MagicMock(return_value=False)
             redmine.get(resource)
-
 
     cache_file = os.path.join(redmine.cache_dir, "{}.json".format(resource))
 
@@ -77,11 +74,10 @@ def test_get_statuses_calls_set_cache_when_no_cache():
     resource = "statuses"
     data = {"statuses": [{"id": 1, "name": "Status"}]}
 
-    with patch.object(Redmine, "fetch", return_value=data) as mock_fetch:
+    with patch.object(Redmine, "fetch", return_value=data):
         with patch.object(Redmine, "set_cache") as mock_cache:
             os.path.exists = MagicMock(return_value=False)
             redmine.get(resource)
-
 
     cache_file = os.path.join(redmine.cache_dir, "{}.json".format(resource))
 
@@ -103,11 +99,10 @@ def test_get_priorities_calls_set_cache_when_no_cache():
     resource = "priorities"
     data = {"priorities": [{"id": 1, "name": "Priority"}]}
 
-    with patch.object(Redmine, "fetch", return_value=data) as mock_fetch:
+    with patch.object(Redmine, "fetch", return_value=data):
         with patch.object(Redmine, "set_cache") as mock_cache:
             os.path.exists = MagicMock(return_value=False)
             redmine.get(resource)
-
 
     cache_file = os.path.join(redmine.cache_dir, "{}.json".format(resource))
 
@@ -129,14 +124,11 @@ def test_get_queries_calls_set_cache_when_no_cache():
     resource = "queries"
     data = {"queries": [{"id": 1, "name": "Query"}]}
 
-    with patch.object(Redmine, "fetch", return_value=data) as mock_fetch:
+    with patch.object(Redmine, "fetch", return_value=data):
         with patch.object(Redmine, "set_cache") as mock_cache:
             os.path.exists = MagicMock(return_value=False)
             redmine.get(resource)
 
-
     cache_file = os.path.join(redmine.cache_dir, "{}.json".format(resource))
 
     mock_cache.assert_called_once_with(cache_file, data)
-
-
