d3871d48c1c73800c44cc637c6d48666958a0d16
Ege Güneş <egegunes@gmail.com>
2018-12-08 00:24:35 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='redminecli',
-    version='1.0.0',
+    version='1.0.1',
     description='Command line interface for Redmine',
     long_description=readme,
     long_description_content_type='text/markdown',
